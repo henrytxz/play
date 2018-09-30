0f7d80ed50c596120cbbbf0f02eb64f837cf5b14
henrytxz <hzhang@pulsepoint.com>
2018-09-30 11:50:50 -0400

    play with json

---
+++ play_with_json/play_with_json.py
@@ -0,0 +1,13 @@
+import json
+
+data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
+
+with open("data_file.json", "w") as write_file:
+    json.dump(data, write_file)
+
+print json.dumps(data, indent=4)
